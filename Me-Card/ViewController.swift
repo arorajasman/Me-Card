c903d23d6cb34677fcc15f1c71b21a78f9c40806
arorajasman <arorajasman97@gmail.com>
2022-11-27 19:38:36 +0530

    adding the files to repository

---
+++ Me-Card/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Me-Card
+//
+//  Created by byteridge on 27/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
